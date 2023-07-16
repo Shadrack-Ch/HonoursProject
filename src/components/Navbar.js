596d49d7dff655c59e69bd3011d107a27969295a
Shadrack-Ch <shady.msuya@yahoo.com>
2023-07-16 19:50:08 -0400

    new idea for nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,38 +5,41 @@ import logo from '../images/Profile_avatar_placeholder_large.png'
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [notifOpen, setNotifOpen] = useState(false);
+  const [userOpen, setUserOpen] = useState(false);
 
   const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const toggleNotifications = () => {
+    setNotifOpen(!notifOpen);
+  };
+
+  const toggleUserMenu = () => {
+    setUserOpen(!userOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="logo">
+      <div className="logo" onClick={toggleUserMenu}>
         <img src={logo} alt="User Icon" />
+        {userOpen && (
+          <div className="dropdown-menu">
+            <Link to="/signin">Sign In</Link>
+            <Link to="/signup">Sign Out</Link>
+          </div>
+        )}
       </div>
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
-        <button type="button">Search</button>
+      <div className="notification-icon" onClick={toggleNotifications}>
+        <img src={logo} alt="Notifications Icon" />
+        {notifOpen && (
+          <div className="dropdown-menu">
+            <p>This is your first notification</p>
+          </div>
+        )}
       </div>
-      <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/signin">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-        </ul>
-      </div>
-      <button className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={handleMenuToggle}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
+      {/* ... Rest of your code ... */}
     </nav>
   );
 }
