7dc6c28a94e45795f21d34cf1e94d13d30a4f1bb
Shadrack-Ch <shady.msuya@yahoo.com>
2023-11-26 17:47:58 -0500

    edit

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -17,19 +17,15 @@ const courseController = require("../controllers/courseController");
 // const authenticate = require('../middleware/authenticate');
 
 // Add New Course
-// Use authentication middleware to ensure only authenticated users can add a course
 router.post('/', /* authenticate, */ courseController.addNewCourse);
 
 // Update Course Details
-// Use authentication middleware to ensure only the owner of the course can update it
 router.put('/:courseId', /* authenticate, */ courseController.updateCourseDetails);
 
 // Get Course Details
-// Use authentication middleware to ensure only users with access can view course details
 router.get('/:courseId', /* authenticate, */ courseController.getCourseDetails);
 
 // Delete Course
-// Use authentication middleware to ensure only the owner of the course can delete it
 router.delete('/:courseId', /* authenticate, */ courseController.deleteCourse);
 
 module.exports = router;
